14587b19a8a7a5901f9f75fbf62caa95d95ea96f
Tiffany LESTROUBAC <46599095+tiffanyLestroubac@users.noreply.github.com>
2021-04-13 09:27:41 +0200

    Create paladin.js

---
+++ paladin.js
@@ -0,0 +1,26 @@
+class Paladin extends Character {
+  constructor(name = 'Ulder', hp = 16, dmg = 3, mana = 160, status, role = 'Paladin') {
+    super(name, hp, dmg, mana, status, role);
+  }
+
+  isSpecialSkillConditionsRespected() {
+    return this.mana >= 40;
+  }
+
+  displaySpecialSkillConditionsViolated() {
+    alert("You don't possess enough mana for this!");
+  }
+
+  displaySpecialSkillInfo() {
+    console.log("Healing Lighting (40 mana points): inflict 4 of damage while healing himself of 5 hp.");
+  }
+
+
+  specialSkill(victim) {
+    this.mana -= 40;
+    this.hp += 5;
+    this.dealDamage(victim, 4);
+    console.log(`${this.name} uses Healing Lightning against ${victim.name}, dealing 4 damage points and heals himself for 5 HP !`);
+  }
+}
+© 2021 GitHub, Inc.
